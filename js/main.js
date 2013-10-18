ac8ac64440b8619aafa541f46b840ab955022e3e
y.bandura <y.bandura@gmail.com>
2013-10-18 17:06:32 +0300

    merge

--- js/main.js
+++ js/main.js
@@ -20,6 +20,9 @@ $(document).ready(function(){
 		cursorfixedheight : 60,
 		autohidemode : false
 	});
+
+	//верхнее меню
+	proccessTopMenu();
 });
 // test data
 var dataBase = [
@@ -115,21 +118,6 @@ var pageList = [
     }
 ];
 
-$(document).ready(function () {
-    $('a.gEmIndexItemImg').mouseenter(function () {
-        $(this).find('.hiddenLink').animate({left: 0});
-    });
-    $('a.gEmIndexItemImg').mouseleave(function () {
-        $(this).find('.hiddenLink').animate({left: -256});
-    });
-    //левое меню
-    $('.gEmSidebarMenuTitle').click(function () {
-        $(this).next('ul.gEmSidebarMenuList').slideToggle(200);
-        $(this).toggleClass('active', 200);
-    });
-    // верхнее меню
-    proccessTopMenu();
-});
 
 function proccessTopMenu() {
     var arr = [];
