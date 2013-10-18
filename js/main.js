79a98e96d0461b858c8c2af560da64cd5eec966e
despotix <despotix@gmail.com>
2013-10-18 16:25:09 +0300

    revert my to latest

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // test data
 var dataBase = [
     {
@@ -250,3 +251,26 @@ function fillListItemByCategory(categoryName, categoryData, categoryItem) {
     }
 }
 */
+=======
+$(document).ready(function(){
+	//fancybox gallery
+	$('.fancybox').fancybox();
+	//левое меню
+	$('.gEmSidebarMenuTitle').click( function(){
+		$(this).next('ul.gEmSidebarMenuList').slideToggle(200);
+		$(this).toggleClass('active',200);
+	});
+	//скролл
+	$(".gEmPhotoGallBox").niceScroll({
+		touchbehavior : false,
+		cursorcolor : "#999999",
+		cursorborder : "none",
+		background : "#ffffff",
+		cursoropacitymax : 1,
+		cursorborderradius : 0,
+		cursorwidth : 5,
+		cursorfixedheight : 60,
+		autohidemode : false
+	});
+});
+>>>>>>> remotes/origin/design
