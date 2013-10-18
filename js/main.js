1a02f0ba09b771d89725f6d5448e661b8cf6262c
y.bandura <y.bandura@gmail.com>
2013-10-18 14:46:18 +0300

    галерея, клик на кота

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
-	$('a.gEmIndexItemImg').mouseenter(function() {
-		$(this).find('.hiddenLink').animate({left:0});
-	});
-	$('a.gEmIndexItemImg').mouseleave( function(){
-		$(this).find('.hiddenLink').animate({left:-256});
-	});
+	//fancybox gallery
+	$('.fancybox').fancybox();
 	//левое меню
 	$('.gEmSidebarMenuTitle').click( function(){
 		$(this).next('ul.gEmSidebarMenuList').slideToggle(200);
 		$(this).toggleClass('active',200);
+	});
+	//скролл
+	$(".gEmPhotoGallBox").niceScroll({
+		touchbehavior : false,
+		cursorcolor : "#999999",
+		cursorborder : "none",
+		background : "#ffffff",
+		cursoropacitymax : 1,
+		cursorborderradius : 0,
+		cursorwidth : 5,
+		cursorfixedheight : 60,
+		autohidemode : false
 	});
 });
