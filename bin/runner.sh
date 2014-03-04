98b94d69bab2a1cea91c537c9fa7ad880e1fb8b3
despotix <despotix@gmail.com>
2014-03-04 17:10:16 +0200

    ..

--- bin/runner.sh
+++ bin/runner.sh
@@ -11,4 +11,4 @@ chown $user:$user $log
 touch $pid
 chown $user:$user $pid
 
-su $user -c "/usr/astra/node/bin/forever $1 -p /home/svc_$provider/ -a -l "$log"/startup.log -o "$log"/startup.log -e "$log"/startup.log --pidFile $pid $service"
+su $user -c "forever $1 -p /home/svc_$provider/ -a -l "$log"/startup.log -o "$log"/startup.log -e "$log"/startup.log --pidFile $pid $service"
